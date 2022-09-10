b4324dc470c0e5473546977e8bdc15d082eba8ad
VyacheslavBakashov <bakashov@bk.ru>
2022-09-10 23:04:50 +0300

    HW_5 added

--- HW_APY_1_import/main.py
+++
@@ -1,9 +0,0 @@
-from application.salary import calculate_salary
-from application.db.people import get_employees
-import datetime as dt
-import netflix
-
-if __name__ == '__main__':
-    print(dt.datetime.now().strftime('%d-%m-%Y %H:%M'))
-    calculate_salary()
-    get_employees()
